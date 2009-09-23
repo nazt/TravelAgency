b82d2664adcfb3effe868c11ee7239a5cb90c048
Nat <nat.weerawan@gmail.com>
2009-09-24 06:39:13 +0700

    Add 3Colours in Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,6 +10,9 @@ class BootStrap {
 		new Engine(engine:'1,500 CC').save()	
 		new Gear(gear:'Automatic').save()
 		new Gear(gear:'Manual').save()
+		new Colour(colour:'Red').save()
+		new Colour(colour:'Blue').save()
+		new Colour(colour:'Green').save()
      }
      def destroy = {
      }
