1d6c9f588424310b99956b287f4112d42ea27bd3
Nat <nat.weerawan@gmail.com>
2009-09-24 11:59:13 +0700

    add Gear to CarStock domain

--- grails-app/domain/CarStock.groovy
+++ grails-app/domain/CarStock.groovy
@@ -1,11 +1,12 @@
 class CarStock {
-	static belongsTo = [colour:Colour,brand:CarBrand,engine:Engine,fuel:Fuel,type:CarType]
+	static belongsTo = [colour:Colour,brand:CarBrand,engine:Engine,fuel:Fuel,type:CarType,gear:Gear]
 	String model
 	String carRegistration
     static constraints = {
 		brand()
 		model()
 		colour()
+		gear()
 		type()
 		engine()
 		fuel()
